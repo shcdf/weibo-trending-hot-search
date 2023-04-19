614d6820c1687466ece280b64bdf25836f57b56b
shcdf <126037992+shcdf@users.noreply.github.com>
2023-04-19 16:09:39 +0800

    0419

---
+++ jieba/0419.py
@@ -0,0 +1,13 @@
+import jieba
+
+import os
+
+# cwd = os.getcwd()  # Get the current working directory (cwd)
+# files = os.listdir(cwd)  # Get all the files in that directory
+# print("Files in %r: %s" % (cwd, files))
+
+
+with open('C:/0000temp/git/weibo-trending-hot-search/jieba/2020-11-24.txt') as f:
+    lines = f.readlines()
+
+print(lines)
