0423d038709413bebeb25343deff7662d76a768e
shcdf <126037992+shcdf@users.noreply.github.com>
2023-04-20 14:09:58 +0800

    jiebacut

--- jieba/02_jiebaCut.py
+++ jieba/02_jiebaCut.py
@@ -1,10 +1,11 @@
+import jieba
 
 import os
 import json
 
-
 f_r = open('C:/0000temp/git/weibo-trending-hot-search/raw/2020-11-24.json', encoding='utf-8')
 f_w = open('C:/0000temp/git/weibo-trending-hot-search/jieba/2020-11-24.txt',"w", encoding='utf-8')
+f_word = open('C:/0000temp/git/weibo-trending-hot-search/jieba/word.txt',"w", encoding='utf-8')
 
 raw_list = json.load(f_r)
 
@@ -14,5 +15,10 @@ for dic in raw_list:
 
     f_w.write(dic['title'] + '\n')
 
+    seg_list = jieba.cut(titlleContent, cut_all=False)  # 精确模式
+    for word in seg_list:
+        print(word)
+        f_word.write(word + '\n')
 
 f_w.close()
+f_word.close()
